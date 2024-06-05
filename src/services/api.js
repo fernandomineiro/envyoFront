8188c2234fecf5b71d8244063ffe3872f16dec07
Fernando Fernandes <fernando@parceirogoogle.com.br>
2024-06-05 12:27:41 -0300

    up

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: process.env.REACT_APP_BACKEND_URL,
+	baseURL: 'https://api.envyo.com.br',
 	withCredentials: true,
 });
 
 export const openApi = axios.create({
-	baseURL: process.env.REACT_APP_BACKEND_URL
+	baseURL: 'https://api.envyo.com.br'
 });
 
 export default api;
